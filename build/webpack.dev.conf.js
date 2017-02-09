b930b798dcf5f868c5893e037fa54270ad87ef50
zhangshibiao <zhangshibiao@jiedaibao.com>
2017-02-09 10:45:22 +0800

    add mock data

---
+++ build/webpack.dev.conf.js
@@ -0,0 +1,46 @@
+var utils = require('./utils')
+var webpack = require('webpack')
+var config = require('../config')
+var merge = require('webpack-merge')
+var baseWebpackConfig = require('./webpack.base.conf')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+var apiJson = require('../api/api.json');
+var TEST_ENV = 'serve';
+var ReplaceBundleStringPlugin = require('replace-bundle-webpack-plugin');
+// add hot-reload related code to entry chunks
+Object.keys(baseWebpackConfig.entry).forEach(function (name) {
+  baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
+})
+
+module.exports = merge(baseWebpackConfig, {
+  module: {
+    rules: utils.styleLoaders({
+      sourceMap: config.dev.cssSourceMap
+    })
+  },
+  // cheap-module-eval-source-map is faster for development
+  devtool: '#cheap-module-eval-source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': config.dev.env
+    }),
+    new ReplaceBundleStringPlugin([{
+      partten: /{{(.+Api)}}/g,
+      replacement: function (match, apiName) {
+        console.log(apiJson[apiName] && apiJson[apiName][TEST_ENV]);
+        return apiJson[apiName] && apiJson[apiName][TEST_ENV];
+      }
+    }]),
+    // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    // https://github.com/ampedandwired/html-webpack-plugin
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true
+    }),
+    new FriendlyErrorsPlugin()
+  ]
+})
